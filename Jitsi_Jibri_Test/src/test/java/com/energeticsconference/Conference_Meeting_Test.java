0c62a0d6bf83c38ce73521c2130f879868924ab2
Arunachalam V <yupitsav56@gmail.com>
2024-07-03 19:18:51 +0530

    Added Files via Upload
    First Commit

---
+++ Jitsi_Jibri_Test/src/test/java/com/energeticsconference/Conference_Meeting_Test.java
@@ -0,0 +1,125 @@
+package com.energeticsconference;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.github.javafaker.Faker;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Conference_Meeting_Test {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		
+		options.addArguments("use-fake-ui-for-media-stream");
+		
+		ChromeDriver driver = new ChromeDriver(options);
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	    
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		
+		Faker faker = new Faker();
+        
+        driver.manage().window().maximize();
+        
+        driver.manage().deleteAllCookies();
+        
+        driver.navigate().refresh();
+        
+        String url = "https://conference-customer-site-dev.fromaround.com/#/login";
+		
+		driver.get(url);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(),'Sign Up')]"))).click();
+		
+		String firstName = faker.name().firstName();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='user_name']"))).sendKeys(firstName);
+		
+		String emailAddress = faker.internet().emailAddress();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='email']"))).sendKeys(emailAddress);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='password']"))).sendKeys("Qa@12345");
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='reType']"))).sendKeys("Qa@12345");
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Continue')]"))).click();
+		
+		for (int i = 1; i <= 6; i++) {
+
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//input[@type='text'])[" + i + "]"))).sendKeys("0");
+
+		}
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Verify')]"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='meet_sec active']"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='topic']"))).sendKeys(firstName);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Create')]"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Launch Meeting')]"))).click();
+		
+		driver.switchTo().frame("jitsiConferenceFrame0");
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='css-1hbmoh1-actionButton primary']"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='subject-text--content css-h56vee-content']")));
+		
+		WebElement meeting_name = driver.findElement(By.xpath("//div[@class='subject-text--content css-h56vee-content']"));
+		
+		String meeting_text = meeting_name.getText();
+		
+		System.out.println(meeting_text);
+		
+		for (int count = 1; count <=9; count++) {
+		
+		((JavascriptExecutor) driver).executeScript("window.open()");
+		
+		ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+		
+		driver.switchTo().window(tabs.get(count));
+		
+		driver.get(url);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='meet_sec active']"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='topic']"))).sendKeys(firstName);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Create')]"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Launch Meeting')]"))).click();
+		
+		driver.switchTo().frame("jitsiConferenceFrame0");
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='css-1hbmoh1-actionButton primary']"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='subject-text--content css-h56vee-content']")));
+		
+		WebElement meeting_name01 = driver.findElement(By.xpath("//div[@class='subject-text--content css-h56vee-content']"));
+		
+		String meeting_text01 = meeting_name01.getText();
+		
+		System.out.println(meeting_text01);
+		
+		
+	}
+
+}
+	
+}	
